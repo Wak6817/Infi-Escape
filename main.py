c098ee1a91aa90f316590fab2b5af720b0a36e19
Wak6817 <benjaminmorris.claus@gmail.com>
2025-08-20 17:33:04 +0200

    Created a input-handler and clear-screen function

--- main.py
+++ main.py
@@ -1,25 +1,56 @@
 import time
+import sys
+import select
+import termios
+import tty
+import os
+
 
 class Engine:
 
     class Backend:
-        pass
+
+        @staticmethod
+        def cls_term():
+            if os.name == 'nt':
+                os.system('cls')
+            else:
+                os.system('clear')
+
+        @staticmethod
+        def input_handler():
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            input_data = ""
+            try:
+                tty.setraw(fd)
+                while True:
+                    Engine.Backend.cls_term()
+                    if select.select([sys.stdin], [], [], 0.1)[0]:
+                        input_data = sys.stdin.read(1)
+                        break
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+            return input_data
+
+        @staticmethod
+        def generate_level(number=None, difficulty=None, type=None):
+            data = {
+                "level_number": number,
+                "difficulty": difficulty,
+                "puzzle_type": type
+            }
+            return data
 
     class Frontend:
         pass
+
 
 class Game:
 
     class Backend:
 
-        @staticmethod
-        def level(difficulty=None):
-            data = {
-                "level_number": 1,
-                "difficulty": difficulty
-            }
-            return data
-        
         @staticmethod
         def user(username=None):
             data = {
@@ -37,9 +68,11 @@ class Game:
             remaining = user_data["time_left"] - int(elapsed)
             return max(remaining, 0)
 
-
     class Frontend:
         pass
 
-    if __name__ == "__main__":
-        pass
+
+if __name__ == "__main__":
+    Engine.Backend.cls_term()
+    input("Maximize your window and press enter.")
+    Engine.Backend.cls_term()
